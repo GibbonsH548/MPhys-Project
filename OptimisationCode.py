d1d2ee10a8c726da09be84505a061f65dea2d134
GibbonsH548 <65288127+GibbonsH548@users.noreply.github.com>
2023-01-18 13:29:02 +0000

    Update OptimisationCode.py
    Introduced the use of the input file
    
    - Now produces either a df of energies and coordinates and/or txt file of energy basin coordinates based on T or F statements from the input file

--- OptimisationCode.py
+++ OptimisationCode.py
@@ -2,32 +2,60 @@ import numpy as np
 import pandas as pd
 from scipy import optimize
 import Energy_Derivative_New as EDF
+import tomli
 
-import time
+with open("input.toml", "rb") as f:
+    input = tomli.load(f)
+
+N = input["simulation_properties"]["particles"]   
+it = input["simulation_properties"]["minimization_iterations"]   
+txt_output = input["simulation_properties"]["output_basin_txt_file"]   
+pd_output = input["simulation_properties"]["output_pd_energy_and_coordinates_file"]
 
 rng = np.random.default_rng() # can seed here if needed
-for i in range(100):
-    N = 50
-    xy = rng.normal(loc = 0, scale = 30, size = 2*N)
-    z = rng.normal(loc = 0, scale = 1, size = N)
-    x0 = np.append(xy, z)
-    #print(EDF.V_total(x0))
+for i in range(it):
+    X = rng.normal(loc = 0, scale = 1000, size = N) # N positions of coordinates in x cartesian directions
+    Y = rng.normal(loc = 0, scale = 1000, size = N) # N positions of coordinates in y cartesian directions
+    Z = rng.normal(loc = 0, scale = 10, size = N) # N positions of coordinates in z cartesian directions
+    x0 = np.array([X,Y,Z]).transpose().flatten() # setting it to [x1, y1, z1,... , xN, yN, zN]
 
-    #print(x0)#
-    #start_time = time.time()
-    res = optimize.minimize(EDF.V_total, x0, method="bfgs", jac=EDF.V_total_dx_array, options={"gtol": 1e-100})  # , jac = "3-point",options = {'gtol':1e-10000})#,jac = V_total_dx, options = {'gtol':1e-10000})#,jac = V_dx_array) #jac = V_dx_array
-    #print("My program took", time.time() - start_time, "to run")
+    res = optimize.minimize(EDF.V_total, x0, method="bfgs", jac=EDF.V_total_dx_array, options = {'gtol':1e-100000})  # , jac = "3-point" - numerical approximation
     e = EDF.V_total(res.x)
-    #print(e)
 
     if i == 0:
         position_min_test = np.split(res.x,len(res.x)/3)
-
         Test = pd.DataFrame({"energy": [e], "positions": [res.x]})
 
     position_min_test = np.concatenate([position_min_test, np.split(res.x,len(res.x)/3)])
     Test.loc[i] = [e, res.x]
     print(i)
 
-#print(Test["positions"][0][1])
-Test.to_pickle("testdatatesting50_i.pkl")
+if pd_output == "T":
+    name = "Outputs/pd_outputs/DataframeFor{}Atoms_{}itterations".format(N,it)
+    filename = "%s.pkl" % name
+    Test.to_pickle(filename)
+
+
+if txt_output == "T":
+    df = Test
+    df_sorted = df.sort_values(by = "energy").reset_index()
+    df_sorted = df_sorted.drop("index",axis = 1)
+    df_sorted['energy'] = df_sorted['energy'].round(6)
+
+
+    df_new = df_sorted.drop_duplicates(subset=['energy'], keep='first')
+    array = df_new["positions"].to_numpy()
+
+    for i in range(len(array)):
+        a = np.array(np.split(array[i],len(array[i])/3))
+        if i == 0 :
+            thearray = a
+        else: 
+            thearray = np.vstack([thearray,a])
+
+    name = "Outputs/BasinLocations/CoordinatesOf{}AtomsinBasins".format(N)
+    filename = "%s.txt" % name
+    np.savetxt(filename,thearray, header=str(len(array)), comments="")
+
+
+
