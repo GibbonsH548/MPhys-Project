014898a065bab0aba35d5b111ccc0b109b7c5363
GibbonsH548 <65288127+GibbonsH548@users.noreply.github.com>
2023-01-28 17:26:53 +0000

    Added option to output ground state configuration for qmc code
    Added an option to output ground state configuration in the format needed for the input gaussian centres when running qmc code

--- OptimisationCode.py
+++ OptimisationCode.py
@@ -11,12 +11,13 @@ N = input["simulation_properties"]["particles"]
 it = input["simulation_properties"]["minimization_iterations"]   
 txt_output = input["simulation_properties"]["output_basin_txt_file"]   
 pd_output = input["simulation_properties"]["output_pd_energy_and_coordinates_file"]
+gs_output = input["simulation_properties"]["ground_state_coordinates_for_QMC"]
 
 rng = np.random.default_rng() # can seed here if needed
 for i in range(it):
-    X = rng.normal(loc = 0, scale = 1000, size = N) # N positions of coordinates in x cartesian directions
-    Y = rng.normal(loc = 0, scale = 1000, size = N) # N positions of coordinates in y cartesian directions
-    Z = rng.normal(loc = 0, scale = 10, size = N) # N positions of coordinates in z cartesian directions
+    X = rng.normal(loc = 0, scale = 10, size = N) # N positions of coordinates in x cartesian directions
+    Y = rng.normal(loc = 0, scale = 10, size = N) # N positions of coordinates in y cartesian directions
+    Z = rng.normal(loc = 0, scale = 1, size = N) # N positions of coordinates in z cartesian directions
     x0 = np.array([X,Y,Z]).transpose().flatten() # setting it to [x1, y1, z1,... , xN, yN, zN]
 
     res = optimize.minimize(EDF.V_total, x0, method="bfgs", jac=EDF.V_total_dx_array, options = {'gtol':1e-100000})  # , jac = "3-point" - numerical approximation
@@ -28,7 +29,7 @@ for i in range(it):
 
     position_min_test = np.concatenate([position_min_test, np.split(res.x,len(res.x)/3)])
     Test.loc[i] = [e, res.x]
-    print(i)
+    #print(i)
 
 if pd_output == "T":
     name = "Outputs/pd_outputs/DataframeFor{}Atoms_{}itterations".format(N,it)
@@ -56,6 +57,26 @@ if txt_output == "T":
     name = "Outputs/BasinLocations/CoordinatesOf{}AtomsinBasins".format(N)
     filename = "%s.txt" % name
     np.savetxt(filename,thearray, header=str(len(array)), comments="")
+
+if gs_output == "T":
+    df = Test
+    df_sorted = df.sort_values(by = "energy").reset_index()
+    df_sorted = df_sorted.drop("index",axis = 1)
+    array = df_sorted["positions"][0]
+    ones = np.ones(len(array),dtype = object)[:,None]
+    array2 = array.reshape(len(array),1)
+    # print(array2)
+
+    thearray = np.concatenate((array2, ones),axis=1)#,dtype=object)
+
+    #print(thearray)
+    #print(gs_output)
+
+    name = "Outputs/GroundStateCoordinatesforQMC/CoordinatesOfGroundState{}".format(N)
+    filename = "%s.txt" % name
+    np.savetxt(filename,thearray,fmt= ['%1.16E','%d'], comments="")
+    # print(df_sorted)
+    # print(df_sorted["positions"][0])
 
 
 
