f335da626dffde60d269d183664d178e93fbac74
GibbonsH548 <65288127+GibbonsH548@users.noreply.github.com>
2023-02-14 12:03:59 +0000

    bug fix
    Fixing some bugs
    - In calculating d-d term

--- Energy_Derivative_New.py
+++ Energy_Derivative_New.py
@@ -1,5 +1,6 @@
+#!/usr/bin/env python
+
 import numpy as np
-from numba import njit
 from scipy.spatial import distance as sd
 import tomli
 
@@ -37,11 +38,23 @@ def V_dd(R):
     ----------
         R: 2D numpy array - shape (N,3)
     """
-    dist_vect = R.reshape(R.shape[0], 1, 3) - R # Subtracts all particle postions from all others gives displacement vectors in a np array (np.newaxis increases the dimension of the array from 2 -> 3) 
+    dist_vect = R.reshape(R.shape[0], 1, 3) - R # Subtracts all particle postions from all others gives displacement vectors in a np array so that you get 3D array of shape (N,N,3) [[[x_1-x_1, y_1-y_1, z_1-z_1],[x_1-x_2, y_1-y_2, z_1-z_2]...]]
+    dist_2 = dist_vect**2 # Takes the square of the difference between each coordinates e.g (x_1 - x_2) -> (x_1 - x_2)^2
+    r_ij = np.sum(dist_2,axis = 2) # converts to 2D np array  - sums the differences between particles in x y and z diresction [(x_1-x_2)^2, (y_1-y_2)^2, (z_1-z_2)^2]  ->  (x_1-x_2)^2 + (y_1-y_2)^2 + (z_1-z_2)^2 
 
-    distance_array = sd.pdist(R)   # Array of distances between particles (for 3 particles [r12,r13,r23])
-    V_dd_1 = np.sum(distance_array**-3) # Calulates the sum of all rij^-3
-    V_dd_2 = np.sum(np.sum(dist_vect*e_hat, axis = 2)**2)/2
+    np.fill_diagonal(r_ij, 1) # adds 1's to diagonal to avoid divide by 0 errors
+    total_dist_3=r_ij**(-3/2)  # calculates |r_ij|^-3 for each combination of i and j
+    np.fill_diagonal(total_dist_3, 0) # changes 1's on diagonals back to 0's (diagonal is r_11 etc. always 0)  
+
+    V_dd_1 = np.sum(total_dist_3)/2 # Calulates the sum of all rij^-3 and divides by 2 to avoid double counting 
+
+
+    total_dist_5=r_ij**(-5/2) # calculates |r_ij|^-5 for each combination of i and j
+    np.fill_diagonal(total_dist_5, 0) # changes 1's on diagonals back to 0's (diagonal is r_11 etc. always 0)  
+    dis_e = np.sum(dist_vect*e_hat, axis = 2)**2 # square of e_hat dot r_ij
+    # print(dist_vect*e_hat)
+    V_dd_2 = (np.sum(dis_e*total_dist_5)/2) # sum of all the previous terms divide by 2 to stop double counting
+
     V_dd = (C_dd)*(V_dd_1-3*V_dd_2) 
     
     return V_dd
@@ -65,8 +78,10 @@ def V_total(x0):
         x0: 1D numpy array - shape (N*3)  
                            - [x1, y1, z1, ..., xN, yN, zN]
     """
+
     R = np.array(np.split(x0,len(x0)/3), dtype=float)  # Splitting x0 into a 2D np array - [[x1, y1, z1],...,[xN, yN, zN]]
-    V_tot = V_trap(R)   + V_dd(R) + V_repulsive(R)  # Suming over all contributions to the overall potential energy
+
+    V_tot = V_repulsive(R) + V_dd(R)  + V_dd(R)  # Sum of all potential terms
     return V_tot
 
 
@@ -88,10 +103,10 @@ def V_rep_dx(dist_vect, dist_2):
         R: 2D numpy array - shape (N,3)
     # """
 
-    r_ij = np.sum(dist_2,axis = 2)
+    r_ij = np.sum(dist_2,axis = 2) # converts to 2D np array  - sums the differences between particles in x y and z diresction [(x_1-x_2)^2, (y_1-y_2)^2, (z_1-z_2)^2]  ->  (x_1-x_2)^2 + (y_1-y_2)^2 + (z_1-z_2)^2 
 
     np.fill_diagonal(r_ij, 1)
-    total_dist_5=r_ij**((rep_order-2)/2)
+    total_dist_5=r_ij**((rep_order-2)/2) # calculates |r_ij|^-rep for each combination of i and j
     np.fill_diagonal(total_dist_5, 0)
 
     X = dist_vect.transpose().transpose((0,2,1))
@@ -106,7 +121,7 @@ def V_dd_dx(dist_vect, dist_2):
     """ Returns an array of the derivatives of the dipolar interaction potential energy term for the system with respect to each parameter (ie [d/dx1,d/dy1,d/dz1,...,d/dxN,d/dyN,d/dzN]V_rep])
     Parameters  
     ----------
-        R: 2D numpy array - shape (N,3)
+        dist_vect: 2D numpy array - shape (N,3)
     """
     r_ij = np.sum(dist_2,axis = 2)
 
@@ -142,5 +157,5 @@ def V_total_dx_array(x0):
     R = np.array(np.split(x0,len(x0)/3), dtype=float)
     dist_vect = R.reshape(R.shape[0], 1, 3) - R # Subtracts all particle postions from all others gives displacement vectors in a np array (np.newaxis increases the dimension of the array from 2 -> 3) 
     dist_2 = dist_vect**2 # square of distances 3D
-    V_dx_array = V_trap_dx(R)  + V_dd_dx(dist_vect, dist_2) + V_rep_dx(dist_vect, dist_2)
+    V_dx_array = V_trap_dx(R)   + V_rep_dx(dist_vect, dist_2)  #+ V_dd_dx(dist_vect, dist_2)
     return V_dx_array
